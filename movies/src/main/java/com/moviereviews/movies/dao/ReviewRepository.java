2ea6cff4880520f1eeb18de28d1af764f540e6d8
Carlos-gitjub <79362942+Carlos-gitjub@users.noreply.github.com>
2024-07-07 00:05:35 +0100

    Add files via upload
    Adds folders for the controllers, repositories, entities and services. 
    
    Changes the `Review` class. Sets the `collection` attribute of the `@Document` annotation  to "reviews" instead of "movies". This way, and thanks to the @DocumentReference annotation of the `Movie` Class, the `createReview` method of the `ReviewService` class inserts a new review document in reviews collection and pushes an object containing the review’s id and body into the Movie document’s reviews array.

---
+++ movies/src/main/java/com/moviereviews/movies/dao/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.moviereviews.movies.dao;
+
+import com.moviereviews.movies.entities.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository <Review, ObjectId> {
+}
---
+++ movies/src/main/java/com/moviereviews/movies/services/MovieService.java
@@ -0,0 +1,23 @@
+package com.moviereviews.movies.services;
+
+import com.moviereviews.movies.dao.MovieRepository;
+import com.moviereviews.movies.entities.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
